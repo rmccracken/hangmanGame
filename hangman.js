cd259bfc20bc8e77dfac84b19d1eb1092b8ba33e
rmccracken <30883836+rmccracken@users.noreply.github.com>
2017-11-11 09:09:11 -0500

    Add files via upload

--- hangman.js
+++ hangman.js
@@ -1,5 +1,4 @@
-//GLOBAL VARIABLES
-//---------------------------------------
+
 // Used to record how many times a letter can be pressed
 var azletter = ['a','b','c','d','e','f','g','h','i','j','k','l',
 				  'm','n','o','p','q','r','s','t','u','v','w','x','y','z'];
